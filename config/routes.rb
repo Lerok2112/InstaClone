2c08fdbe79051dab1ba2266896d871cf45ef2ed3
Lerok <ovseychuk2000@mail.ru>
2020-03-26 12:58:22 +0100

    LAB_2 added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
   resources :users
   get 'home/index'
   root to: "home#index"
+
+
 end
---
+++ db/migrate/20200326115714_la_la.rb
@@ -0,0 +1,5 @@
+class LaLa < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :locked_at, :datetime
+  end
+end
